5afbe1b3d825da381b8fb7d065d8eb2fc52680f3
Mudit Pandey <Muditpanday17@gmail.com>
2025-04-18 10:12:21 +0530

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -1,7 +1,11 @@
 import { Prisma } from '@prisma/client';
 
 // Export types from Prisma schema
+// @typescript-eslint/no-empty-object-type
 export type Course = Prisma.CourseGetPayload<{}>;
+// @typescript-eslint/no-empty-object-type
 export type Module = Prisma.ModuleGetPayload<{}>;
+// @typescript-eslint/no-empty-object-type
 export type Lesson = Prisma.LessonGetPayload<{}>;
+// @typescript-eslint/no-empty-object-type
 export type User = Prisma.UserGetPayload<{}>;
